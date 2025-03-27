c1b2dbd8d0c1c50a253841b89e86c92550d2b102
DaniilSlabov <daniilSlabov>
2025-03-27 14:55:58 +0200

    Specify appId

--- src/config/sanboxContextOptions.ts
+++ src/config/sanboxContextOptions.ts
@@ -1,5 +1,5 @@
 export const sanboxContextOptions = {
   baseUrl: "https://sandbox.frontegg.com",
   clientId: "9af126b9-c35f-4e2d-a3f1-c261e22aaf4a",
-  appId: "xxxx",
+  appId: "ad6012f5-905f-430e-ad0d-64e85f0ba6c7",
 };
